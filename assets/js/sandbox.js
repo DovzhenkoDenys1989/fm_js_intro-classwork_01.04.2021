b78e5160c4e1166732eac3ec719863472802f715
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-04 18:38:08 +0300

    Add Accumulator example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,9 +1,27 @@
-//Написать сумму произвольного количества чисел
-function getSum(){
-  let result = null;
-  for(let i = 0; i < arguments.length; i++){
-    result += +arguments[i];
-  }
+/*
+Напишите функцию-конструктор Accumulator(startingValue).
+Объект, который она создаёт, должен уметь следующее:
+Хранить «текущее значение» в свойстве value. 
+Начальное значение устанавливается в аргументе конструктора startingValue.
+Метод read() использует prompt для получения числа и прибавляет его к свойству value.
+Таким образом, свойство value является текущей суммой всего,
+что ввёл пользователь при вызовах метода read(),
+с учётом начального значения startingValue. 
+*/
 
-  return result;
+function Accumulator(startingValue){
+  this.value = isFinite(startingValue) ? +startingValue : 0;
 }
+
+Accumulator.prototype = new AccumulatorPrototype();
+
+function AccumulatorPrototype(){
+  this.read = function read(){
+    const userNumber = +prompt("Введите число: ");
+    if(!isFinite(userNumber)) return this.value;
+
+    return this.value += userNumber;
+  }
+}
+
+const accumulator = new Accumulator();
