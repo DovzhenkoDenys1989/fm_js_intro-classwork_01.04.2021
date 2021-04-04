ab7eab850e9a908e7a74461863dcf4cf81d0ef5d
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-04 16:40:53 +0300

    Add student constructor example

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const student1 = {
+  id: 1,
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 20,
+  isMale: true,
+}
+
+function Student(firstName, lastName, age, isMale){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+}
+
+Student.prototype = new StudentPrototype();
+
+function StudentPrototype(){
+  this.toString = function toString(){
+    return `${this.firstName} ${this.lastName}. I'm ${this.age} years old `;
+  }
+  this.sayHello = function sayHello(){
+    return `${this.firstName} ${this.lastName} says hello!`;
+  }
+  this.say = function say(message){
+    return `Студент ${this.firstName} говорит:\n${message}`;
+  }
+}
+/*
+  say() метод, принимающий строку и возвращающий строку
+  в таком формате:
+  Студент <Имя студента> говорит:
+  <содержимое принятой строки>
+*/
+
+const student2 = new Student('Bob', 'Black', 25, true);
---
+++ assets/js/sandbox.js
@@ -0,0 +1,9 @@
+//Написать сумму произвольного количества чисел
+function getSum(){
+  let result = null;
+  for(let i = 0; i < arguments.length; i++){
+    result += +arguments[i];
+  }
+
+  return result;
+}
